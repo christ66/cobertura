f7504c0f1520232019e0ec5d7d9a44b2a102b7d9
thekingant <thekingant@3373f243-8d0f-0410-a8fe-cd033897b1b5>
2005-03-17 21:57:33 +0000

    Forgot the copyright notice.

--- cobertura/test/net/sourceforge/cobertura/reporting/xml/JUnitXMLParserErrorHandler.java
+++ cobertura/test/net/sourceforge/cobertura/reporting/xml/JUnitXMLParserErrorHandler.java
@@ -1,3 +1,23 @@
+/*
+ * Cobertura - http://cobertura.sourceforge.net/
+ *
+ * Copyright (C) 2005 Mark Doliner <thekingant@users.sourceforge.net>
+ *
+ * Cobertura is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * Cobertura is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cobertura; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
 
 package net.sourceforge.cobertura.reporting.xml;
 
@@ -19,16 +39,19 @@ public class JUnitXMLParserErrorHandler implements ErrorHandler
 
 	public void error(SAXParseException exception)
 	{
+		exception.printStackTrace();
 		Assert.fail(exception.toString());
 	}
 
 	public void fatalError(SAXParseException exception)
 	{
+		exception.printStackTrace();
 		Assert.fail(exception.toString());
 	}
 
 	public void warning(SAXParseException exception)
 	{
+		exception.printStackTrace();
 		Assert.fail(exception.toString());
 	}
 
