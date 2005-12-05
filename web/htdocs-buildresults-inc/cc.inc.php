9ce9e33e9f567e183dbc042016c0c5b68c071b09
(no author) <(no author)@3373f243-8d0f-0410-a8fe-cd033897b1b5>
2005-12-05 19:22:26 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_7'.

--- web/htdocs-buildresults-inc/cc.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include("/home/groups/c/co/cobertura/htdocs-buildresults-inc/util.inc.php");
-include("/home/groups/c/co/cobertura/htdocs-buildresults-inc/cc_util.inc.php");
-include("/home/groups/c/co/cobertura/htdocs-buildresults-inc/navigation.inc.php");
-
-?>
