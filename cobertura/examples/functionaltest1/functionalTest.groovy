3e4f6fbae73e54069dde05e2b58c3b16d7fbbcab
lewijw <lewijw@3373f243-8d0f-0410-a8fe-cd033897b1b5>
2009-01-16 19:13:18 +0000

    Add functional tests for cobertura-check.

--- cobertura/examples/functionaltest1/functionalTest.groovy
+++ cobertura/examples/functionaltest1/functionalTest.groovy
@@ -52,6 +52,8 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
+ 
+import org.apache.tools.ant.BuildException
 
 
 //load testUtil definitions
@@ -68,3 +70,97 @@ runReports = { set ->
 	ant."${reportTaskName}"([datafile:'${basedir}/cobertura.ser', destdir:'${coverage.html.dir}', maxmemory:'512M'], set)
 
 }
+ 
+checkRates = {
+	/*
+	 * Read the xml report to get the expected values
+	 */
+	def datafile = new File("${ant.project.baseDir}/cobertura.ser")
+	def xmlReport = new File("${ant.project.baseDir}/${ant.project.properties.'coverage.xml.dir'}/coverage.xml")
+	 
+	def parser = new XmlParser()
+	parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd",false)
+	def root = parser.parse(xmlReport)
+	
+	def totalLineRate = rateToInt(root.'@line-rate')
+	def totalBranchRate = rateToInt(root.'@branch-rate')
+	def minPackageLineRate = rateToInt(root.packages.'package'.'@line-rate'*.toDouble().min().toString())
+	def minPackageBranchRate = rateToInt(root.packages.'package'.'@branch-rate'*.toDouble().min().toString())
+	def minClassLineRate = rateToInt(root.packages.'package'.classes.'class'.'@line-rate'*.toDouble().min().toString())
+	def minClassBranchRate = rateToInt(root.packages.'package'.classes.'class'.'@branch-rate'*.toDouble().min().toString())
+	
+	def ratesMap = [
+			totallinerate:totalLineRate,
+			totalbranchrate:totalBranchRate,
+			packagelinerate:minPackageLineRate,
+			packagebranchrate:minPackageBranchRate,
+			linerate:minClassLineRate,
+			branchrate:minClassBranchRate
+	]
+	
+	/*
+	 * Do a check with all the values - this should pass
+	 */
+	ant.echo(message:"Doing a check with ${ratesMap} - it should pass")
+	def tempMap = [datafile:datafile]
+	tempMap.putAll(ratesMap)
+	ant."${checkTaskName}"(tempMap)
+
+	/*
+	 * Now do a check with each rate individually.   Add one to make sure it fails.
+	 * 
+	 * For those rates that equal 100, there is no way the check can fail, so do
+	 * a check to make sure it passes.
+	 */
+	ratesMap.each { rateType, rateValue ->
+		if (rateValue == 100) {
+			ant.echo(message:"Doing a check with ${rateType}=100 - it should pass")
+			ant."${checkTaskName}"(datafile:datafile, (rateType):rateValue)
+		} else {
+			ant.echo(message:"Doing a check with ${rateType}=${rateValue+1} - it should fail")
+			assertCheckFailure("Check of ${rateType} with value ${rateValue + 1} should have failed") {
+				ant."${checkTaskName}"(datafile:datafile, (rateType):rateValue + 1)
+			}
+		}
+	}
+	
+	if (!ratesMap.values().any { it < 50 }) {
+		ant.fail(message:"This test is expecting one rate to be under 50.   Otherwise the following test will fail.")
+	}
+	 
+	/*
+	 * Now do a check that does not specify any rates.   They should default to 50%.
+	 * This should fail.
+	 */
+	ant.echo(message:"Doing a check using the defaults - it should fail")
+	assertCheckFailure("Check with defaults should fail") {
+		 ant."${checkTaskName}"(datafile:datafile)
+	 }
+}
+ 
+ 
+rateToInt = { doubleString ->
+	//multiply rate by 100 and drop the decimals
+	(Double.parseDouble(doubleString) * 100.0).intValue()
+}
+ 
+ 
+assertCheckFailure = { errorMessage, closure ->
+	boolean failed = false
+	try {
+		closure.call()
+	} catch (BuildException e) {
+		failed = true
+	}
+	if (!failed) {
+		ant.fail(message:errorMessage)
+	}
+}
+
+
+ 
+ 
+ 
+ 
+ 
+ 
