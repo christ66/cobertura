63873303772023d1b499c82cf1daa02e46686290
lewijw <lewijw@3373f243-8d0f-0410-a8fe-cd033897b1b5>
2009-08-27 18:13:32 +0000

    Copy from https://cobertura.svn.sourceforge.net/svnroot/cobertura/vendor/javancss to https://cobertura.svn.sourceforge.net/svnroot/cobertura/trunk/cobertura/javancss.

---
+++ cobertura/javancss/test/Test92.java
@@ -0,0 +1,20 @@
+package lama.karma;
+import java.io.*;
+
+/**
+ * Data driven (and optionally interactive) testing harness to exercise regular
+ * expression compiler and matching engine.
+ *
+ * @author <a href="mailto:jonl@muppetlabs.com">Jonathan Locke</a>
+ * @author <a href="mailto:jon@latchkey.com">Jon S. Stevens</a>
+ * @version $Id: Test92.java 15 2008-08-04 22:00:07Z hboutemy $
+ */
+public class Test92
+{
+    void show()
+    {
+        say("\n-----------------------\n");
+        //say("Expression #" + n + " \"" + expr + "\" ");
+        say("Expression #" + (n) + " \"" + expr + "\" ");
+    }
+}
