c94fb4bfd935ad3a521f4e3da10572d15d4ce3c5
schristou88 <schristou88@gmail.com>
2013-04-10 17:54:16 -0500

    Move to maven project

---
+++ cobertura/src/test/java/net/sourceforge/cobertura/ant/ConditionCalls.java
@@ -0,0 +1,10 @@
+package net.sourceforge.cobertura.ant;
+
+public class ConditionCalls {
+	public static final int CALL_CONDITION_LINE_NUMBER = 17;
+	public static final int CALL_IGNORE_LINE_NUMBER = 21;
+	public static final int LOOKUP_SWITCH_LINE_NUMBER = 39;
+	public static final int TABLE_SWITCH_LINE_NUMBER = 56;
+	public static final int MULTI_CONDITION_LINE_NUMBER = 74;
+	public static final int MULTI_CONDITION2_LINE_NUMBER = 82;
+}
