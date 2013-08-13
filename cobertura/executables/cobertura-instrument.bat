0ecbba5cf9b251889388b080430f8f5049a08540
John Ferguson <jferguson@siemens.com>
2013-08-13 11:43:59 -0400

    Corrected the paths to the jars so this bat file runs.

--- cobertura/executables/cobertura-instrument.bat
+++ cobertura/executables/cobertura-instrument.bat
@@ -18,4 +18,4 @@ shift
 goto getArgs
 :doneStart
 
-java -cp "%COBERTURA_HOME%cobertura.jar;%COBERTURA_HOME%lib\asm-3.3.1.jar;%COBERTURA_HOME%lib\asm-tree-3.3.1.jar;%COBERTURA_HOME%lib\asm-commons-3.3.1.jar;%COBERTURA_HOME%lib\log4j-1.2.9.jar;%COBERTURA_HOME%lib\jakarta-oro-2.0.8.jar" net.sourceforge.cobertura.instrument.Main %CMD_LINE_ARGS%
+java -cp "%COBERTURA_HOME%cobertura-2.0.3.jar;%COBERTURA_HOME%lib\asm-4.1.jar;%COBERTURA_HOME%lib\asm-util-4.1.jar;%COBERTURA_HOME%lib\asm-tree-4.1.jar;%COBERTURA_HOME%lib\asm-commons-4.1.jar;%COBERTURA_HOME%lib\asm-analysis-4.1.jar;%COBERTURA_HOME%lib\log4j-1.2.9.jar;%COBERTURA_HOME%lib\oro-2.0.8.jar" net.sourceforge.cobertura.instrument.Main %CMD_LINE_ARGS%
