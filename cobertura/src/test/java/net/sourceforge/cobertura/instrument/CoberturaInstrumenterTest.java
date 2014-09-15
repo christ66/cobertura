1374052221dd17bf78db078cb54eb0fb9824c414
Alexander Kampmann <s9alkamp@stud.uni-saarland.de>
2014-09-15 15:12:44 +0200

    activated fail on error for the instrumenter test

--- cobertura/src/test/java/net/sourceforge/cobertura/instrument/CoberturaInstrumenterTest.java
+++ cobertura/src/test/java/net/sourceforge/cobertura/instrument/CoberturaInstrumenterTest.java
@@ -44,15 +44,7 @@ public class CoberturaInstrumenterTest {
 	public void instrumentExisting() throws ClassNotFoundException {
 		coberturaInstrumenter.setDestinationDirectory(new File("target",
 				"build"));
-		coberturaInstrumenter.addInstrumentationToSingleClass(new File(
-				"src/test/resources/coberturaInstrumenterTest/Test1.class"));
-	}
-	
-	@Test
-	public void instrumentFailing() throws ClassNotFoundException {
 		coberturaInstrumenter.setFailOnError(true);
-		coberturaInstrumenter.setDestinationDirectory(new File("target",
-				"build"));
 		coberturaInstrumenter.addInstrumentationToSingleClass(new File(
 				"src/test/resources/coberturaInstrumenterTest/Test1.class"));
 	}
