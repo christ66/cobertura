5e57b0a85494c32551559de24aec1e7e3d277a81
Dennis Lundberg <dennisl@apache.org>
2014-11-19 13:56:23 +0100

    Cobertura needs an SLF4J binding in compile scope, fixes #203

--- cobertura/executables/cobertura-report.bat
+++ cobertura/executables/cobertura-report.bat
@@ -18,4 +18,4 @@ shift
 goto getArgs
 :doneStart
 
-java -cp "%COBERTURA_HOME%cobertura-${project.version}.jar;%COBERTURA_HOME%lib\asm-${asmVersion}.jar;%COBERTURA_HOME%lib\asm-tree-${asmVersion}.jar;%COBERTURA_HOME%lib\asm-commons-${asmVersion}.jar;%COBERTURA_HOME%lib\slf4j-api-${slf4jVersion}.jar;%COBERTURA_HOME%lib\oro-${oroVersion}.jar" net.sourceforge.cobertura.reporting.ReportMain %CMD_LINE_ARGS%
+java -cp "%COBERTURA_HOME%cobertura-${project.version}.jar;%COBERTURA_HOME%lib\asm-${asmVersion}.jar;%COBERTURA_HOME%lib\asm-tree-${asmVersion}.jar;%COBERTURA_HOME%lib\asm-commons-${asmVersion}.jar;%COBERTURA_HOME%lib\slf4j-api-${slf4jVersion}.jar;%COBERTURA_HOME%lib\logback-core-${logbackVersion}.jar;%COBERTURA_HOME%lib\logback-classic-${logbackVersion}.jar;%COBERTURA_HOME%lib\oro-${oroVersion}.jar" net.sourceforge.cobertura.reporting.ReportMain %CMD_LINE_ARGS%
