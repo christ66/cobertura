5bde937db0504ccb1bbcead2ce9a32ebb21b14eb
Joze Rozanec <jmrozanec@gmail.com>
2019-03-08 23:23:45 +0100

    Implement recommendations by @cobexer at #264

--- cobertura/executables/cobertura-check.sh
+++ cobertura/executables/cobertura-check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 COBERTURA_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-java -cp $COBERTURA_HOME/coberturatura-2.1.1.jar:$COBERTURA_HOME/lib/asm-5.0.1.jar:$COBERTURA_HOME/lib/asm-analysis-5.0.1.jar:$COBERTURA_HOME/lib/asm-tree-5.0.1.jar:$COBERTURA_HOME/lib/asm-commons-5.0.1.jar:$COBERTURA_HOME/lib/asm-util-5.0.1.jar:$COBERTURA_HOME/lib/slf4j-api-1.7.5.jar:$COBERTURA_HOME/lib/logback-core-1.0.13.jar:$COBERTURA_HOME/lib/logback-classic-1.0.13.jar:$COBERTURA_HOME/lib/oro-2.0.8.jar net.sourceforge.cobertura.instrument.InstrumentMain $*
+java -cp $COBERTURA_HOME/coberturatura-${project.version}.jar:$COBERTURA_HOME/lib/asm-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-analysis-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-tree-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-commons-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-util-${asmVersion}.jar:$COBERTURA_HOME/lib/slf4j-api-${slf4jVersion}.jar:$COBERTURA_HOME/lib/logback-core-${logbackVersion}.jar:$COBERTURA_HOME/lib/logback-classic-${logbackVersion}.jar:$COBERTURA_HOME/lib/oro-${oroVersion}.jar net.sourceforge.cobertura.instrument.InstrumentMain "$@"
--- cobertura/executables/cobertura-merge.sh
+++ cobertura/executables/cobertura-merge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 COBERTURA_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-java -cp $COBERTURA_HOME/cobertura-${project.version}.jar:$COBERTURA_HOME/lib/asm-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-tree-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-commons-${asmVersion}.jar:$COBERTURA_HOME/lib/slf4j-api-${slf4jVersion}.jar:$COBERTURA_HOME/lib/logback-core-${logbackVersion}.jar:$COBERTURA_HOME/lib/logback-classic-${logbackVersion}.jar:$COBERTURA_HOME/lib/oro-${oroVersion}.jar net.sourceforge.cobertura.merge.MergeMain $*
+java -cp $COBERTURA_HOME/cobertura-${project.version}.jar:$COBERTURA_HOME/lib/asm-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-tree-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-commons-${asmVersion}.jar:$COBERTURA_HOME/lib/slf4j-api-${slf4jVersion}.jar:$COBERTURA_HOME/lib/logback-core-${logbackVersion}.jar:$COBERTURA_HOME/lib/logback-classic-${logbackVersion}.jar:$COBERTURA_HOME/lib/oro-${oroVersion}.jar net.sourceforge.cobertura.merge.MergeMain "$@"
