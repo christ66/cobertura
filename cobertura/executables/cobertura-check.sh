3cc9dca85ce3025e90cc3bbeb668f608e312c750
Christoph Obexer <cobexer@gmail.com>
2019-03-12 18:43:35 +0100

    update executable scripts because of bad merge

--- cobertura/executables/cobertura-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-COBERTURA_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-java -cp $COBERTURA_HOME/coberturatura-${project.version}.jar:$COBERTURA_HOME/lib/asm-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-analysis-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-tree-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-commons-${asmVersion}.jar:$COBERTURA_HOME/lib/asm-util-${asmVersion}.jar:$COBERTURA_HOME/lib/slf4j-api-${slf4jVersion}.jar:$COBERTURA_HOME/lib/logback-core-${logbackVersion}.jar:$COBERTURA_HOME/lib/logback-classic-${logbackVersion}.jar:$COBERTURA_HOME/lib/oro-${oroVersion}.jar net.sourceforge.cobertura.instrument.InstrumentMain "$@"
